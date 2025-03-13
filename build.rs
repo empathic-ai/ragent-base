ed24a55dce1548c7e6d2b2d2114e7fdc7c7de2de
Catchawink <threeeasywords@gmail.com>
2025-03-13 12:21:50 -0400

    Updating files.

--- build.rs
+++ build.rs
@@ -2,23 +2,24 @@
 use std::{env, path::PathBuf};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.extern_path(".ragent.Thing", "::flux::prelude::Thing");
-    config.extern_path(".ragent.Dynamic", "::flux::prelude::Dynamic");
-    
-    let attribute = "#[derive(documented::Documented, serde::Serialize, serde::Deserialize)]";
-    if let Ok(_) = env::var("CARGO_FEATURE_TONIC") {
-        let mut builder = tonic_build::configure();
+    if let Ok(_) = env::var("CARGO_FEATURE_PROST") {
+        let mut config = prost_build::Config::new();
+        config.extern_path(".ragent.Thing", "::flux::prelude::Thing");
+        config.extern_path(".ragent.Dynamic", "::flux::prelude::Dynamic");
+        
+        let attribute = "#[derive(documented::Documented, serde::Serialize, serde::Deserialize)]";
+        if let Ok(_) = env::var("CARGO_FEATURE_TONIC") {
+            let mut builder = tonic_build::configure();
 
-        if let Ok(_) = env::var("CARGO_FEATURE_BEVY") {
-            builder = builder.type_attribute(".", "#[derive(bevy::prelude::Reflect, bevy::prelude::Event, ragent_derive::Task)]");
+            if let Ok(_) = env::var("CARGO_FEATURE_BEVY") {
+                builder = builder.type_attribute(".", "#[derive(bevy::prelude::Reflect, bevy::prelude::Event, ragent_derive::Task)]");
+            }
+
+            builder.type_attribute(".", attribute).compile_with_config(config, &["proto/ragent.proto"], &["proto"])?;
+        } else {
+            config.out_dir(PathBuf::from(std::env::var("OUT_DIR").unwrap()));
+            config.type_attribute(".", attribute).compile_protos(&["proto/ragent.proto"], &["proto"]);
         }
-
-        builder.type_attribute(".", attribute).compile_with_config(config, &["proto/ragent.proto"], &["proto"])?;
-    } else {
-        config.out_dir(PathBuf::from(std::env::var("OUT_DIR").unwrap()));
-        config.type_attribute(".", attribute).compile_protos(&["proto/ragent.proto"], &["proto"]);
     }
-
     Ok(())
 }
